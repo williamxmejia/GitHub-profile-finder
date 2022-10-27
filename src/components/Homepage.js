b3c3ffd2c0568eb3f68c58d0718e71e7d3d6383c
williamxmejia <willx1995@gmail.com>
2022-10-27 01:39:50 -0500

    feat: styled profile page and display error message

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -28,7 +28,8 @@ const Navbar = (props) => {
       console.log(results);
       setData(results);
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      alert("No User Found, try williamxmejia")
     }
   };
 
@@ -50,10 +51,10 @@ const Navbar = (props) => {
             <form
               onSubmit={(e) => {
                 getSingle();
-                if(search === ""){
+                if (search === "") {
                   getGitHub();
                 }
-                
+
                 e.preventDefault();
               }}
             >
@@ -81,22 +82,44 @@ const Navbar = (props) => {
         <h1 className="text-center mb-5">
           Welcome to the GitHub Profile Finder
         </h1>
+        {/* {error && (
+          <div>No user Found</div>
+        )} */}
         {!Array.isArray(data) && (
-          <div key={data} className="card col-2 mx-3 mb-5">
-            <img
-              className="card-img-top"
-              src={data.avatar_url}
-              alt="Card image cap"
-            />
-            <div className="card-body fw-bold text-uppercase text-center">
-              {data.login}
+          <div className="d-flex">
+            <div key={data.id} className="card col-3 mx-5 mb-5 mt-2">
+              <div>
+                <img
+                  className="card-img-top"
+                  src={data.avatar_url}
+                  alt="Card image cap"
+                />
+                <div className="card-body fw-bold text-center">
+                  <p className="text-uppercase">{data.login}</p>
+                  <p>
+                    <a href={`https://github.com/${data.login}`}>
+                      GitHub Account
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="text-white">
+              <h2>Name: {data.name}</h2>
+              <h2>Bio: {data.bio}</h2>
+              <h2>
+                Blog: <a href={data.blog}>{data.blog}</a>
+              </h2>
+              <h2>Followers: {data.followers}</h2>
+              <h2>Following: {data.following}</h2>
+              <h2>Repos: {data.public_repos}</h2>
+              <h2>ID: {data.id}</h2>
             </div>
           </div>
         )}
 
         {data.length > 20 && (
           <div className="d-flex flex-wrap justify-content-center">
-            {/* <div>{data.login}</div> */}
             {data.map((user) => (
               <div key={user.id} className="card col-2 mx-3 mb-5">
                 <img
@@ -105,7 +128,8 @@ const Navbar = (props) => {
                   alt="Card image cap"
                 />
                 <div className="card-body fw-bold text-uppercase text-center">
-                  {user.login}
+                  <p>{user.login}</p>
+                  <p>ID: {user.id}</p>
                 </div>
               </div>
             ))}
